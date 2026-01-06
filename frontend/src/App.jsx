6771c18d3a3051781f77959945eaba1f6648dd75
MSCPerera <164726262+MSCPerera@users.noreply.github.com>
2026-01-06 21:49:46 +0530

    Update project initiation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,22 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 import LeaveApplicationPage from './pages/LeaveApplicationPage'
-import './App.css'
+import AdminDashboard from './pages/AdminDashboard'
+
 
 function App() {
-  return <LeaveApplicationPage />
+  return (
+    <>
+        <Routes>
+
+          {/* Public route - Employee Leave Application */}
+          <Route path="/" element={<LeaveApplicationPage />} />
+
+          {/* Admin protected route */}
+          <Route path="/admin" element={<AdminDashboard />} />
+        </Routes>
+    </> 
+  )
 }
 
-export default App
+export default App;
